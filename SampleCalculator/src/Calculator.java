06ff0b2f7c4dd1221afb01477b329e9b12d3538b
Sabarinadh2002 <140910234+Sabarinadh2002@users.noreply.github.com>
2024-09-22 13:24:07 +0530

    Added extra buttons and set hover and clicked colors

--- SampleCalculator/src/Calculator.java
+++ SampleCalculator/src/Calculator.java
@@ -11,8 +11,10 @@ import java.awt.Insets;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -31,38 +33,61 @@ public class Calculator extends JFrame implements ActionListener{// actionlisten
 	
 	JLabel displaylabel;
 	JButton sevenbutton,eightbutton,ninebutton,fourbutton,fivebutton,sixbutton,threebutton,
-	twobutton,onebutton,dotbutton,minusbutton,plusbutton,multibutton,dividebutton,zerobutton
-	,equalbutton,clearbutton,squareButton,rootButton;
+	twobutton,onebutton,dotbutton,minusbutton,plusbutton,multibutton,dividebutton,zerobutton,piButton
+	,equalbutton,clearbutton,squareButton,rootButton,backspaceButton,negateButton,percentageButton,logButton;
     
 	
 	
 	public Calculator() {// constructor
 		setTitle("Calculator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 600);
+        setSize(300, 500);
         setLocationRelativeTo(null);
 
 	    CalculatorPanel mainPanel = new CalculatorPanel();
-		mainPanel.setLayout(new BorderLayout(0,0));
+		mainPanel.setLayout(new BorderLayout(5,5));
 		mainPanel.setBackground(Color.decode("#202020"));
 
+		JPanel topPanel = new JPanel();
+		topPanel.setLayout(new BorderLayout());
+		topPanel.setBackground(Color.decode("#202020"));
+		topPanel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+
+		JLabel headingLabel = new JLabel("Sample Calculator");
+		headingLabel.setFont(new Font("Arial", Font.CENTER_BASELINE, 16));
+		headingLabel.setForeground(Color.white);
+		headingLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		headingLabel.setPreferredSize(new Dimension(300, 30));
+		topPanel.add(headingLabel, BorderLayout.NORTH);
 		
+
 		displaylabel = new JLabel("0");
         displaylabel.setFont(new Font("Arial", Font.BOLD, 50));
         displaylabel.setHorizontalAlignment(SwingConstants.RIGHT);
         displaylabel.setBackground(Color.decode("#202020"));
         displaylabel.setOpaque(true);
         displaylabel.setForeground(Color.white);
-        
-        displaylabel.setPreferredSize(new Dimension(400, 100));
-		mainPanel.add(displaylabel, BorderLayout.NORTH);
+		displaylabel.setPreferredSize(new Dimension(300, 50));
+		topPanel.add(displaylabel, BorderLayout.CENTER);
+
+		mainPanel.add(topPanel, BorderLayout.NORTH);
 		
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(6, 4, 5, 5));
+        buttonPanel.setLayout(new GridLayout(6, 4, 3, 3));
         buttonPanel.setBackground(Color.decode("#202020"));
+		buttonPanel.setBorder(BorderFactory.createEmptyBorder(1,1,20,1));
 
 
         // Create and add NumberButtons
+
+		negateButton = createOperatorButton("±");
+		buttonPanel.add(negateButton);
+		percentageButton = createOperatorButton("%");
+		buttonPanel.add(percentageButton);
+		logButton = createOperatorButton("lg");
+		buttonPanel.add(logButton);
+		backspaceButton = createOperatorButton("←");
+        buttonPanel.add(backspaceButton);
 
 		sevenbutton = createNumberButton("7");
         buttonPanel.add(sevenbutton);
@@ -70,8 +95,10 @@ public class Calculator extends JFrame implements ActionListener{// actionlisten
         buttonPanel.add(eightbutton);
         ninebutton = createNumberButton("9");
         buttonPanel.add(ninebutton);
-        dividebutton = createOperatorButton("/");
-        buttonPanel.add(dividebutton);
+		piButton = createOperatorButton("π");
+		buttonPanel.add(piButton);
+		
+        
 
 		fourbutton = createNumberButton("4");
         buttonPanel.add(fourbutton);
@@ -91,31 +118,37 @@ public class Calculator extends JFrame implements ActionListener{// actionlisten
         minusbutton = createOperatorButton("-");
         buttonPanel.add(minusbutton);
 
-        zerobutton = createOperatorButton("0");
-        buttonPanel.add(zerobutton);
+        
         dotbutton = createOperatorButton(".");
         buttonPanel.add(dotbutton);
-        equalbutton = createFunctionButton("=");
-        buttonPanel.add(equalbutton);
-        plusbutton = createOperatorButton("+");
+		zerobutton = createOperatorButton("0");
+        buttonPanel.add(zerobutton);
+		plusbutton = createOperatorButton("+");
         buttonPanel.add(plusbutton);
+		clearbutton = createOperatorButton("C");
+        buttonPanel.add(clearbutton);
 
 		squareButton = createOperatorButton("X²");
 		buttonPanel.add(squareButton);
 		rootButton = createOperatorButton("√");
 		buttonPanel.add(rootButton);
+		dividebutton = createOperatorButton("/");
+        buttonPanel.add(dividebutton);
 
-        clearbutton = createOperatorButton("C");
-        buttonPanel.add(clearbutton);
+		equalbutton = createFunctionButton("=");
+        buttonPanel.add(equalbutton);
+		
+
+        
 
 		JPanel wrapperPanel = new JPanel(new BorderLayout());
 		wrapperPanel.setBackground(Color.decode("#202020"));
 
-		wrapperPanel.setBorder(new javax.swing.border.EmptyBorder(5,5,-50,5));
+		
 		wrapperPanel.add(buttonPanel, BorderLayout.CENTER);
 
 	    // Add the button panel to the main panel
-        mainPanel.add(wrapperPanel, BorderLayout.CENTER);
+        mainPanel.add(wrapperPanel, BorderLayout.SOUTH);
 
         // Set the main panel as the content pane
         setContentPane(mainPanel);
@@ -127,7 +160,10 @@ public class Calculator extends JFrame implements ActionListener{// actionlisten
 
 	// Helper methods to create buttons
     private JButton createNumberButton(String text) {
-        RoundedButton button = new RoundedButton(text, Color.decode("#3b3b3b"), 20, 20);
+		Color defaultColor = Color.decode("#3b3b3b"); // Default gray
+        Color hoverColor = Color.decode("#505050");   // Darker gray on hover
+        Color clickColor = Color.decode("#808080");
+        RoundedButton button = new RoundedButton(text, defaultColor,hoverColor,clickColor,20, 20);
         button.setFont(new Font("Arial", Font.BOLD, 30));
 		button.setForeground(Color.WHITE);
 		
@@ -136,8 +172,11 @@ public class Calculator extends JFrame implements ActionListener{// actionlisten
     }
 
     private JButton createOperatorButton(String text) {
-        RoundedButton button = new RoundedButton(text, Color.decode("#333332"), 20, 20);
-        button.setFont(new Font("Arial", Font.BOLD, 30));
+		Color defaultColor = Color.decode("#333332"); // Default dark
+		Color hoverColor = Color.decode("#666666");   // Lighter on hover
+		Color clickColor = Color.decode("#999999");   // Even lighter on click
+		RoundedButton button = new RoundedButton(text, defaultColor, hoverColor, clickColor, 20, 20);
+		button.setFont(new Font("Arial", Font.BOLD, 30));
         button.setForeground(Color.WHITE);
 		
 		button.addActionListener(this);
@@ -145,9 +184,12 @@ public class Calculator extends JFrame implements ActionListener{// actionlisten
     }
 
     private JButton createFunctionButton(String text) {
-        RoundedButton button = new RoundedButton(text, Color.decode("#da9fe5"), 20, 20);
+        Color defaultColor = Color.decode("#da9fe5"); // Default purple
+        Color hoverColor = Color.decode("#e0b0f0");   // Brighter purple on hover
+        Color clickColor = Color.decode("#b07fb3");   // Darker purple on click
+        RoundedButton button = new RoundedButton(text, defaultColor, hoverColor, clickColor, 20, 20);
         button.setFont(new Font("Arial", Font.PLAIN, 30));
-        button.setForeground(Color.WHITE);
+        button.setForeground(Color.black);
 		
 		button.addActionListener(this);
         return button;
@@ -202,7 +244,33 @@ public void actionPerformed(ActionEvent e) { // Switches
 		handleSquareClick();
 	} else if (e.getSource() == rootButton){
 		handleRootClick();
+	} else if (e.getSource() == backspaceButton){
+		handleBackSpaceClick();
+	} else if (e.getSource() == piButton){
+		displaylabel.setText(String.valueOf(Math.PI));
+	} else if (e.getSource() == logButton){
+		try{
+			double value = Double.parseDouble(displaylabel.getText());
+			displaylabel.setText(String.valueOf(Math.log10(value)));
+		} catch (NumberFormatException ex){
+			displaylabel.setText("Invalid input");
+		}
+	}else if (e.getSource() == negateButton) {
+		try {
+			double value = Double.parseDouble(displaylabel.getText());
+			displaylabel.setText(String.valueOf(value * -1));
+		} catch (NumberFormatException ex){
+			displaylabel.setText("Invalid input");
+		}
+	}else if (e.getSource() == percentageButton){
+		try {
+			double value = Double.parseDouble(displaylabel.getText());
+			displaylabel.setText(String.valueOf(value / 100));
+		} catch (NumberFormatException ex){
+			displaylabel.setText("Invalid input");
+		}
 	}
+
 }
 private void handleNumberClick(String value) {
 	if (IsOperatorClicked) {
@@ -217,6 +285,15 @@ private void handleOperatorClick(String operator) {
 	IsOperatorClicked = true;
 	OldValue = displaylabel.getText();
 	this.operator = operator;
+}
+
+private void handleBackSpaceClick(){
+	String currentText = displaylabel.getText();
+	if (currentText.length() > 1){
+		displaylabel.setText(currentText.substring(0, currentText.length() - 1));
+	} else {
+		displaylabel.setText("0");
+	}
 }
 
 private void handleEqualClick() {
@@ -283,31 +360,65 @@ private void handleRootClick(){
 }
 
 class RoundedButton extends JButton {
-	private Color backgroundColor;
+	
 	private int arcWidth;
 	private int arcHeight;
+	private Color hoverBackgroundColor;
+	private Color clickBackgroundColor;
+	private Color defaultBackgroundColor;
 
-	public RoundedButton (String text, Color backgroundColor, int arcWidth, int arcHeight){
+	public RoundedButton (String text, Color backgroundColor,Color hoveColor,Color clickColor, int arcWidth, int arcHeight){
 		super(text);
-		this.backgroundColor = backgroundColor;
+		
+		this.defaultBackgroundColor = backgroundColor;
 		this.arcWidth = arcWidth;
 		this.arcHeight = arcHeight;
+		this.hoverBackgroundColor = backgroundColor.brighter();
+		this.clickBackgroundColor = backgroundColor.darker();
+		
 		setFocusPainted(false);
 		setBorderPainted(false);
 		setOpaque(false);
 		setContentAreaFilled(false);
+
+		setBackground(defaultBackgroundColor);
+
+	addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            setBackground(hoverBackgroundColor);
+        }
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			setBackground(defaultBackgroundColor);
+		}
+		@Override
+		public void mousePressed(MouseEvent e){
+			setBackground(clickBackgroundColor);
+		}
+		@Override
+		public void mouseReleased(MouseEvent e){
+			setBackground(hoverBackgroundColor);
+		}
+	});
 	}
 	@Override
 	protected void paintComponent(Graphics g){
 		Graphics2D g2 = (Graphics2D) g.create();
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		g2.setColor(backgroundColor);
+		g2.setColor(getBackground());
 		g2.fillRoundRect(0, 0, getWidth(), getHeight(), arcWidth, arcHeight);
 
 		super.paintComponent(g2);
 
 		g2.dispose();
+	}
+	@Override
+	public void setBackground(Color bg){
+		super.setBackground(bg);
+		repaint();
 	}
 
 	@Override
